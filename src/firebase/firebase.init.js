84c8757e10a2982d39ad08ddb91e84cda3b94cc2
Abdul Malek <abdulmalekmonir97@gmail.com>
2025-09-18 23:55:27 +0600

    fix security issues

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAvnIuLKV4ITQnG-l4IasqRmTtogmWVgTU",
-  authDomain: "fir-common-auth.firebaseapp.com",
-  projectId: "fir-common-auth",
-  storageBucket: "fir-common-auth.firebasestorage.app",
-  messagingSenderId: "230362962405",
-  appId: "1:230362962405:web:9d8539c80ad226fa8bb70c",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
